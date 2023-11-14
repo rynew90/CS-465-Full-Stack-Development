7ebe883393858206420a970d2dfc57754a3b4e93
Ryne Williams <rynew90@yahoo.com>
2023-11-14 06:10:20 -0600

    module3

--- app_server/controllers/travel.js
+++ app_server/controllers/travel.js
@@ -4,7 +4,7 @@ var trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 /* GET travel view */
 const travel = (req, res) => {
-    res.render('travel', { title: 'Travlr Getaways', trips});
+    res.render('travel', { title: 'Travlr Getaways', trips });
 };
 
 module.exports = {
--- public/app.js
+++ public/app.js
@@ -7,6 +7,9 @@ const logger = require('morgan');
 const indexRouter = require('./app_server/routes/index');
 const usersRouter = require('./app_server/routes/users');
 const travelRouter = require('./app_server/routes/travel');
+const roomRouter = require('./app_server/routes/rooms');
+const mealRouter = require('./app_server/routes/meals');
+const newsRouter = require('./app_server/routes/news');
 
 const app = express();
 
@@ -27,6 +30,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/travel', travelRouter);
+app.use('/rooms', roomRouter);
+app.use('/meals', mealRouter);
+app.use('/news', newsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
